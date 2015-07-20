064e055f0cc856b027ce1c80a6da1d16a1ac4b20
Jérome Perrin <jerome@nexedi.com>
2015-07-20 13:26:30 +0200

    Release 0.50

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.50.dev0'
+version = '0.50'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
