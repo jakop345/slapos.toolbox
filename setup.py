05745c1e3c07681d27f50c4526f058ffe9d7d0a7
Nicolas Wavrant <nicolas.wavrant@nexedi.com>
2016-09-16 11:24:39 +0200

    Release 0.59

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.58'
+version = '0.59'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
