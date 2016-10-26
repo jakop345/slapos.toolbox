f66a0190a7887afc2f1b7e2f2bda0756b19686ea
Rafael Monnerat <rafael@nexedi.com>
2016-10-26 13:29:57 +0200

    Release 0.61

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.60'
+version = '0.61'
 name = 'slapos.toolbox'
 long_description = open("README.rst").read() + "\n"
 
